fd230a0a74408db07fd89d31bf767bcee142527e
Charlie Jade <summer.alex07@gmail.com>
2013-12-01 17:18:47 +0800

    finish the first page listing, and the right navigation bar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
 console.log('\'Allo \'Allo!');
+
+
+
+function showAlert() {
+    $("#myAlert").addClass("in");
+}
+window.setTimeout(function () {
+    showAlert();
+}, 3000);
+function hideAlert() {
+    $("#myAlert").removeClass("in");
+}
+window.setTimeout(function () {
+    hideAlert();
+}, 6000);
